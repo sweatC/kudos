e02c89f8c4d58d6d5171312177120f717c988124
Ilya N <1llyanovohatskyy@gmail.com>
2018-01-16 13:40:06 +0200

    Revert "added welcome-screen.test.js and snaps"
    This reverts commit 74a0289d7a1473eee9887784287dc84a843d557d.

--- __tests__/snap/App.test.js
+++ __tests__/snap/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import App from '../../App';
+
 import renderer from 'react-test-renderer';
 
-
-it('App renders without crashing', () => {
+it('renders without crashing', () => {
   const rendered = renderer.create(<App />).toJSON();
-  expect(rendered).toMatchSnapshot();
+  expect(rendered).toBeTruthy();
 });
--- __tests__/snap/welcome-screen.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import WelcomeScreen from '../../screens/welcome-screen';
-import renderer from 'react-test-renderer';
-
-
-it('Welcome screen renders without crashing', () => {
-    const rendered = renderer.create(<WelcomeScreen />).toJSON();
-    expect(rendered).toMatchSnapshot();
-});
